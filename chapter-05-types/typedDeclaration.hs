e0dea28a98fe0d7e3e821507d37e6a664501493f
André Formento <andreformento.sc@gmail.com>
2017-05-03 22:34:29 -0300

    chapter-05-types

---
+++ chapter-05-types/typedDeclaration.hs
@@ -0,0 +1,10 @@
+module TypeDeclaration where
+
+-- Prelude> let fifteenInt = fifteen :: Int
+-- Prelude> let fifteenDouble = fifteen :: Double
+
+-- Prelude> let f1 :: Num a => a -> a -> a; f1 x y = x + y
+-- Prelude> let f2 :: (Num a, Num b) => a -> b -> b; f2 x y = undefined
+
+fifteenInt = fifteen :: Int
+fifteenDouble = fifteen :: Double
