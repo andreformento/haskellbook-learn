6132048a26307cfb8cd76a6e6e55d9b56e070d66
andre.formento <andre.formento@concrete.com.br>
2017-05-30 09:37:58 -0300

    Resolve exercises

--- course/exercises.hs
+++ course/exercises.hs
@@ -1,3 +1,4 @@
+module Exercises where
 -- exercises
 
 -- escrever o lambda na mão (beta reduction)
@@ -5,9 +6,19 @@
 -- example: lambda sintax
 -- let sub = \x y -> x - y
 
--- sub x y = x - y
--- add x y = x + y
--- mul x y = x * y
+sub x y = x - y
+add x y = x + y
+mul x y = x * y
 
--- f x = (add x x) + (sub x 10)
--- g x y = (mul x y) / (sub x y)
+f x = (add x x) + (sub x 10)
+g x y = (mul x y) / (sub x y)
+
+myLambda = \x y -> x - y
+
+withWhere :: Int -> Int
+withWhere x = z + k where z = (add x x)
+                          k = (sub x 10)
+
+withLet x =
+    let z = (add x x)
+        k = (sub x 10) in z + k
