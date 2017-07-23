d5cb269afb6ae660847fda8100b738454079a480
André Formento <andreformento.sc@gmail.com>
2017-07-23 15:44:49 -0300

    Add exercises

--- chapter-06-typeclasses/chapterExercises.hs
+++ chapter-06-typeclasses/chapterExercises.hs
@@ -29,9 +29,14 @@ data Sentence =
 
 s1 = Sentence "dogs" "drool"
 s2 = Sentence "Julie" "loves" "dogs"
-
+----------------------------------------
 data Rocks =
     Rocks String deriving (Eq, Show)
-
 data Yeah =
     Yeah Bool deriving (Eq, Show)
+data Papu =
+    Papu Rocks Yeah
+    deriving (Eq, Show)
+truth = Papu (Rocks "blablabla")
+             (Yeah True)
+----------------------------------------
